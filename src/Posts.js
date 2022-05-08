90cd7b26202c5ea10bf2327d3909cbb6bd68a2f3
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-05-08 19:02:54 -0300

    o botão de like está implementado 	modified:   src/Posts.js

--- src/Posts.js
+++ src/Posts.js
@@ -1,44 +1,17 @@
+import React from 'react';
+
 export default function Posts(){
+    const users=["meowed","barked"]
+    const userProfile=["assets/img/meowed.svg", "assets/img/barked.svg"]
+    const postImages=["assets/img/dog.svg","assets/img/gato-telefone.svg"]
+    const firstLike=["adorable_animals","respondeai"]
+    const firstLikeProfiles=["assets/img/adorable_animals.svg", "assets/img/respondeai.svg"]
     return (
     <div class="posts">
         
-        <Post />
-        <div class="post">
-          <div class="topo">
-            <div class="usuario">
-              <img src="assets/img/barked.svg" />
-              barked
-            </div>
-            <div class="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
-            </div>
-          </div>
-
-          <div class="conteudo">
-            <img src="assets/img/dog.svg" />
-          </div>
-
-          <div class="fundo">
-            <div class="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-
-            <div class="curtidas">
-              <img src="assets/img/adorable_animals.svg" />
-              <div class="texto">
-                Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        {users.map ((user, index)=> <Post user={user} userProfile={userProfile[index]} postImage={postImages[index]} firstLike={firstLike[index]} firstLikeProfile={firstLikeProfiles[index]} />)}
+        
+    </div>
     );
 }
 function Post(props){
@@ -46,8 +19,8 @@ function Post(props){
         <div class="post">
           <div class="topo">
             <div class="usuario">
-              <img src="assets/img/meowed.svg" />
-              meowed
+              <img src={props.userProfile} />
+              {props.user}
             </div>
             <div class="acoes">
               <ion-icon name="ellipsis-horizontal"></ion-icon>
@@ -55,28 +28,43 @@ function Post(props){
           </div>
 
           <div class="conteudo">
-            <img src="assets/img/gato-telefone.svg" />
+            <img src={props.postImage} />
           </div>
 
           <div class="fundo">
-            <div class="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
+            <PostActions />
 
             <div class="curtidas">
-              <img src="assets/img/respondeai.svg" />
+              <img src={props.firstLikeProfile} />
               <div class="texto">
-                Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                Curtido por <strong>{props.firstLike}</strong> e <strong>outras 101.523 pessoas</strong>
               </div>
             </div>
           </div>
         </div>
-    )
+    );
+}
+const INITIALVALUE="heart-outline"
+function PostActions(props){
+  const [isLiked, setIsLiked] = React.useState(INITIALVALUE);
+  function likeUnlike(){
+    if(isLiked=="heart-outline"){
+      setIsLiked("heart");
+    }
+    if(isLiked=="heart"){
+      setIsLiked("heart-outline");
+    }
+  }
+  return(
+  <div class="acoes">
+    <div>
+      <ion-icon name={isLiked} onClick={likeUnlike}></ion-icon>
+      <ion-icon name="chatbubble-outline"></ion-icon>
+      <ion-icon name="paper-plane-outline"></ion-icon>
+    </div>
+    <div>
+      <ion-icon name="bookmark-outline"></ion-icon>
+    </div>
+  </div>
+  );
 }
